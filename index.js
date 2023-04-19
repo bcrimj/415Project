535163eb765e639e4e3d8c7e7a4969baf18e6eea
bcrimj <77739328+bcrimj@users.noreply.github.com>
2023-04-19 15:47:30 -0500

    phase 2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const app = express();
 const port = 3000;
 var fs = require("fs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const uri =
+  "mongodb+srv://classuser:Silvery1@415test.80wtryo.mongodb.net/?retryWrites=true&w=majority";
 
 const bodyParser = require("body-parser");
 app.use(
@@ -15,7 +19,6 @@ app.use(
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
-
 
 var router = express.Router();
 
@@ -64,15 +67,31 @@ app.get("/", function (req, res) {
 });
 
 app.get("/rest/list", function (req, res) {
-  fs.readFile("data.json", "utf-8", (err, string) => {
-    if (err) {
-      console.log(err);
-      return;
+  // fs.readFile("data.json", "utf-8", (err, string) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return;
+  //   }
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-Type", "application/json");
+  //   res.end(string);
+  // });
+
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      const database = client.db("415Test");
+      const item = database.collection("cmps415");
+
+      const list = await item.find({}, { projection: { _id: 0 } }).toArray();
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(list, null, 2));
+    } finally {
+      await client.close();
     }
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(string);
-  });
+  }
+  run().catch(console.dir);
 });
 
 app.post("/rest/ticket", function (req, res) {
@@ -87,6 +106,8 @@ app.post("/rest/ticket", function (req, res) {
   const recipient = req.body.recipient;
   const submitter = req.body.submitter;
   const aid = req.body.assignee_id;
+  const follower_ids = req.body.follower_ids;
+  const tags = req.body.tags;
 
   var data = {
     id: id,
@@ -99,48 +120,158 @@ app.post("/rest/ticket", function (req, res) {
     status: status,
     recipient: recipient,
     submitter: submitter,
-    assignee_id: aid
-  }
+    assignee_id: aid,
+    follower_ids: follower_ids,
+    tags: tags,
+  };
 
-  const fileData = JSON.parse(fs.readFileSync("data.json"));
-  fileData.push(data);
-  fs.writeFile("data.json", JSON.stringify(fileData, null, 2), (err) => {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      const message = "Data submitted successfully."
-      res.send({
-        'id': id,
-        'created_at': ctime,
-        'updated_at': utime,
-        'type': type,
-        'subject': subject,
-        'description': description,
-        'priority': priority,
-        'status': status,
-        'recipient': recipient,
-        'submitter': submitter,
-        'assignee_id': aid
+  // const fileData = JSON.parse(fs.readFileSync("data.json"));
+  // fileData.push(data);
+  // fs.writeFile("data.json", JSON.stringify(fileData, null, 2), (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+  //   else {
+  //     const message = "Data submitted successfully."
+  //     res.send({
+  //       'id': id,
+  //       'created_at': ctime,
+  //       'updated_at': utime,
+  //       'type': type,
+  //       'subject': subject,
+  //       'description': description,
+  //       'priority': priority,
+  //       'status': status,
+  //       'recipient': recipient,
+  //       'submitter': submitter,
+  //       'assignee_id': aid
+  //     });
+  //   }
+  // })
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      const database = client.db("415Test");
+      const item = database.collection("cmps415");
+      await item.insertOne(data, function (err, resp) {
+        if (err) {
+          console.log(err);
+          return;
+        }
       });
+      res.send(JSON.stringify(data, null, 2));
+    } finally {
+      await client.close();
     }
-  })
+  }
+  run().catch(console.dir);
 });
 
 app.get("/rest/ticket/:id", function (req, res) {
-  fs.readFile("data.json", "utf-8", (err, string) => {
-    if (err) {
-      console.log(err);
-      return;
+  // fs.readFile("data.json", "utf-8", (err, string) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return;
+  //   }
+  //   var data = JSON.parse(string.toString());
+  //   var result = data.filter(function (item) {
+  //     return item.id == req.params.id;
+  //   });
+  //   const final = JSON.stringify(result, null, 2);
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-Type", "application/json");
+  //   res.end(final);
+  // });
+
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      const database = client.db("415Test");
+      const item = database.collection("cmps415");
+      const query = { id: req.params.id };
+      const list = await item.findOne(query, { projection: { _id: 0 } });
+      if (!list) {
+        res.send("That id number is not in the system.");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(list, null, 2));
+      }
+    } finally {
+      await client.close();
     }
-    var data = JSON.parse(string.toString());
-    var result = data.filter(function (item) {
-      return item.id == req.params.id;
-    });
-    const final = JSON.stringify(result, null, 2);
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(final);
-  });
+  }
+  run().catch(console.dir);
 });
 
+app.delete("/rest/ticket/:id", function (req, res) {
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      const database = client.db("415Test");
+      const item = database.collection("cmps415");
+      const query = { id: req.params.id };
+      const result = await item.deleteOne(query);
+      if (result.deletedCount === 1) {
+        res.send("Document deleted successfully.");
+      } else {
+        res.send("No documents with that id number exist in the database.");
+      }
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+});
+
+app.put("/rest/ticket/:id", function (req, res) {
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      const database = client.db("415Test");
+      const item = database.collection("cmps415");
+      const query = { id: req.params.id };
+      const result = await item.findOne(query);
+      if (!result) {
+        res.send("No items match that id number.");
+      } else {
+        const id = req.body.id;
+        const ctime = Date.now();
+        const utime = Date.now();
+        const type = req.body.type;
+        const subject = req.body.subject;
+        const description = req.body.description;
+        const priority = req.body.priority;
+        const status = req.body.status;
+        const recipient = req.body.recipient;
+        const submitter = req.body.submitter;
+        const aid = req.body.assignee_id;
+        const follower_ids = req.body.follower_ids;
+        const tags = req.body.tags;
+
+        var updated = {
+          $set: {
+          id: id,
+          created_at: ctime,
+          updated_at: utime,
+          type: type,
+          subject: subject,
+          description: description,
+          priority: priority,
+          status: status,
+          recipient: recipient,
+          submitter: submitter,
+          assignee_id: aid,
+          follower_ids: follower_ids,
+          tags: tags,
+        }};
+
+        await item.updateOne(query, updated);
+        res.send(JSON.stringify(updated, null, 2));
+      }
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+});
